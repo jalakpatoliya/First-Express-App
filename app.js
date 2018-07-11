cfbda83c656ba9f5cfa0893ab73d3b03458c4c2c
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-07-11 19:34:27 +0500

    some changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express =require('express');
-var app		=express();
+var express = require('express');
+var app		  =	express();
 
+var port = process.env.PORT || 8080;
 //routing on /
 app.get("/",function (req,res) {
 	res.send("Helloo...Kem cho, Khaana Khaake Jaana Hnn. Jai shree Krishna.");
@@ -18,6 +19,6 @@ app.get("/dogs",function(req,res){
 });
 
 //listen
-app.listen(2000,function () {
-	console.log("server started");
+app.listen(port,function () {
+	console.log('Our app is running on http://localhost:' + port);
 })
